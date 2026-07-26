85014c69d4419a2cc42855f25954108a92c5b810
SOTA56 <blueflower.telecaster@gmail.com>
2026-07-26 11:50:56 +0900

    Recover automatically from stale deployments

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
+const root=document.getElementById('root')!
+createRoot(root).render(<StrictMode><App /></StrictMode>)
+requestAnimationFrame(()=>requestAnimationFrame(()=>{
+  if(root.childElementCount>0){
+    document.documentElement.dataset.appReady='true'
+    sessionStorage.removeItem('otoblogic:last-startup-recovery')
+    const url=new URL(location.href)
+    if(url.searchParams.has('__refresh')){
+      url.searchParams.delete('__refresh')
+      history.replaceState(history.state,'',`${url.pathname}${url.search}${url.hash}`)
+    }
+  }
+}))
