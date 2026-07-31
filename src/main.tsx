7d5da105a12188f3f97803ddcfb015bfc7faa922
SOTA56 <blueflower.telecaster@gmail.com>
2026-07-31 20:12:53 +0900

    Prevent false startup recovery after deploys

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ requestAnimationFrame(()=>requestAnimationFrame(()=>{
   if(root.childElementCount>0){
     document.documentElement.dataset.appReady='true'
     sessionStorage.removeItem('otoblogic:startup-recovery-attempted-v2')
+    sessionStorage.removeItem('otoblogic:startup-recovery-attempted-v3')
     sessionStorage.removeItem('otoblogic:last-startup-recovery')
     const url=new URL(location.href)
     if(url.searchParams.has('__refresh')){
